b2218e64bf7956d3c34e0646136beddcc11e5c54
filippo-ceradini <90599585+filippo-ceradini@users.noreply.github.com>
2023-02-09 16:07:57 +0100

    initial commit

---
+++ 00._Miscellaneous/1._Intro_Exercises/1.Basics.js
@@ -0,0 +1,38 @@
+// --------------------------------------
+// Variables, strings, numbers, floats
+// --------------------------------------
+// Exercise 1 - Console and constiables
+
+const firstName = "Anders";
+const lastName = "Latif";
+// EXERCISE
+// show in the console
+// My first name is Anders and my last name is Latif
+
+console.log("My first name is " + firstName + " and my last name is " + lastName);
+
+// --------------------------------------
+// Exercise 2 - Numbers and Strings
+
+const year = "2022";
+const number = 1;
+
+// Add the year plus the number
+// The result should be 2023
+// You cannot touch line 1 or 2
+
+const resultOne = Number(year) + number;
+const resultTwo = parseInt(year) + number;
+const resultThree = +year + number;
+
+console.log(resultThree);
+
+//Number, in case of not number, will return NaN
+console.log(Number("2343dsfdsfds"));
+//parseInt, in case of not number, will return only the numbers inside the string
+console.log(parseInt("2343dsfdsfds"));
+
+
+
+// --------------------------------------
+
